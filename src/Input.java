e45afeed17af354c09278a0e645b103831a25beb
Rafael Oviedo <169010054+rafaeleatstaquitos@users.noreply.github.com>
2025-02-03 03:21:04 +0000

    Done

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Age: ");
+        int age = scanner.nextInt(); 
+
+        System.out.print("Weight: ");
+        float weight = scanner.nextFloat();
+
+        System.out.print("Has Smoked?: ");
+        boolean smoked = scanner.nextBoolean();
+        scanner.close();
+        
+        System.out.printf("\nPatient Info\nName: %s\nAge: %d\nWeight: %.2f\nHas Smoked?: %b\n", name, age, weight, smoked);
+    }
+}
